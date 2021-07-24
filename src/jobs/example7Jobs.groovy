02c7173f638da38cbcf65674ca0bf46f0d457a15
opeomotayo <43703454+opeomotayo@users.noreply.github.com>
2021-07-24 07:00:35 +0100

    Update example7Jobs.groovy

--- src/jobs/example7Jobs.groovy
+++ src/jobs/example7Jobs.groovy
@@ -1,4 +1,4 @@
-import main/groovy/com/dslexample/builder/GradleCiJobBuilder.groovy
+// import com/dslexample/builder/GradleCiJobBuilder.groovy
 
 String basePath = 'example7'
 List developers = ['dev1@example.com', 'dev2@example.com']
@@ -7,16 +7,16 @@ folder(basePath) {
     description 'This example shows how to create jobs using Job builders.'
 }
 
-new GradleCiJobBuilder()
-    .name("$basePath/gradle-project1")
-    .description('An example using a job builder for a Gradle project.')
-    .ownerAndProject('myorg/project1')
-    .emails(developers)
-    .build(this)
+// new GradleCiJobBuilder()
+//     .name("$basePath/gradle-project1")
+//     .description('An example using a job builder for a Gradle project.')
+//     .ownerAndProject('myorg/project1')
+//     .emails(developers)
+//     .build(this)
 
-new GradleCiJobBuilder()
-    .name("$basePath/gradle-project2")
-    .description('Another example using a job builder for a Gradle project.')
-    .ownerAndProject('myorg/project2')
-    .emails(developers)
-    .build(this)
+// new GradleCiJobBuilder()
+//     .name("$basePath/gradle-project2")
+//     .description('Another example using a job builder for a Gradle project.')
+//     .ownerAndProject('myorg/project2')
+//     .emails(developers)
+//     .build(this)
